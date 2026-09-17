92da6829db61833dd3d101f62d740fea2d44e1b5
SarahSpeedy <dubeyharshit0605@gmail.com>
2026-09-17 06:52:56 +0530

    Time: 55 ms (20.13%) | Memory: 49.9 MB (20.91%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-int dp[10010];
 
- int rec(vector<int>& arr, int x){
+
+ int f(int i,int x,vector<vector<int>>&dp,vector<int>&arr){
     if(x==0) return 0;
-    if(x<0) return 1e9;
-    if(dp[x]!=-1) return dp[x];
-    int mini=1e9;
-    for(int i=0;i<arr.size();i++){
-        int ans=rec(arr,x-arr[i]);
-        if(ans!=1e9){
-          mini=min(mini,ans+1);
-        }
+    if(i<0) return 1e9;
+
+    if(dp[i][x]!=-1) return dp[i][x];
+
+    int nottake=f(i-1,x,dp,arr);
+     int take=1e9;
+    if(arr[i]<=x){
+        take=1+f(i,x-arr[i],dp,arr);
     }
-    return dp[x]=mini;
+    return dp[i][x]=min(take,nottake);
  }
 
-    int coinChange(vector<int>& arr, int amount) {
-       fill(dp,dp+10010,-1);
-        int ans=rec(arr,amount);
-        if(ans==1e9) return -1;
+
+
+    int coinChange(vector<int>& arr, int x) {
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(x+1,-1));
+
+        int ans=f(n-1,x,dp,arr);
+
+        if(ans>=1e9) return -1;
+
         return ans;
     }
 };
